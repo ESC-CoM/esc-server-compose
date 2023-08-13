8b7e8cbfd8d1a9ba86b746bf7a65fc2316e5eff8
jsween5723 <jsween5723@gmail.com>
2023-08-14 05:34:43 +0900

    build(compose): add build.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-docker compose down
-while read -r LINE; do
-  if [[ $LINE == *'='* ]] && [[ $LINE != '#'* ]]; then
-    ENV_VAR="$(echo $LINE | envsubst)"
-    eval "export $ENV_VAR"
-  fi
-done < .env
-env
-docker compose up
